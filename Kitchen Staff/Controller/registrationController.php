fe552935d3951e0643eaaa96cd9fb41473268528
rashed-ahammod <ahammodr74@gmail.com>
2026-01-09 20:49:19 +0600

    dbconnection

--- Kitchen Staff/Controller/registrationController.php
+++ Kitchen Staff/Controller/registrationController.php
@@ -1,16 +1,41 @@
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+function regController(){
+
 
    require_once '../../Model/Staff/registrationModel.php';
 
     $name = $_POST['name'];
-    
+    $contactno = $_POST['contactno'];
+    $address = $_POST['address'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $confirmPassword = $_POST['confirmPassword'];
-    $accountType = $_POST['accounttype'];
-
-    
+    $accountType = $_POST['accounttype'];  
+    $user={
+        'name' => $name,
+        'contactno' => $contactno,
+        'address' => $address,
+        'email' => $email,
+        'password' => $password,
+        'confirmPassword' => $confirmPassword,
+        'accountType' => $accountType
+    };
+    $status = regUser($user);
+    if($status){
+        return true;
+    } else {
+        return false;
+    }
+    if($_SERVER['REQUEST_METHOD'] =='POST'){
+        if(regController()){
+            header
+        } else {
+            echo "Registration Failed. Please try again.";
+        }
+    }
+    else {
+        echo "Invalid Request Method.";}
   
 }
 ?>
