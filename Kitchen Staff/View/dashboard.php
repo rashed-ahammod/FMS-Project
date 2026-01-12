c93d3f10db46aa0bcb95d148313954015ea778a7
rashed-ahammod <ahammodr74@gmail.com>
2026-01-13 02:47:22 +0600

    dashboardstart

---
+++ Kitchen Staff/View/dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <title>Kitchen Staff Dashboard</title>
+
+</head>
+<body>
+    <div class="dashboard header">
+        <div class="logo">Online Food Service Management</div>
+        <div class="profile">
+            <span class="profile name">Profile</span>
+            <div class="options">
+                <a href="">My Profile</a>
+                <a href="">Change Password</a>
+                <a href="">Logout</a>
+</div>
+</div>
+</div>
