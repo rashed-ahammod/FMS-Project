ef17bebf3b8c35e63ba0f859a2e53aefb35d1d50
rashed-ahammod <ahammodr74@gmail.com>
2026-01-13 22:17:43 +0600

    updated all

--- Kitchen Staff/View/dashboard.php
+++ Kitchen Staff/View/dashboard.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
+<html>
 <head>
     <title>Kitchen Staff Dashboard</title>
+    <link rel="stylesheet" href="../CSS/dashboard.css">
+
 
 </head>
 <body>
-    <div class="dashboard header">
+    <div class="dashboard_header">
         <div class="logo">Online Food Service Management</div>
         <div class="profile">
             <span class="profile_name">Profile</span>
             <div class="options">
-                <a href="">My Profile</a>
+                <a href="profile.php">My Profile</a>
                 <a href="">Change Password</a>
                 <a href="">Logout</a>
 </div>
@@ -19,23 +22,23 @@
     <h1>Kitchen Staff Dashboard<h1>
         <div class="feature_list">
             <a href="" class ="feature">
-            <h2>Manage Menu<h2>
+            <h2>Manage Menu</h2>
                 <p>Add,Update or Remove Menu Items</p>
 </a>
         <a href="" class ="feature">
-            <h2>Update Order Status<h2>
+            <h2>Update Order Status</h2>
                 <p>Track and Update Order Status</p>
 </a>
         <a href="" class ="feature">
-            <h2>Order History<h2>
+            <h2>Order History</h2>
                 <p>All Completed Order History</p>
 </a>
         <a href="" class ="feature">
-            <h2>Availability<h2>
+            <h2>Availability</h2>
                 <p>Set Food item wether availabe or unavailabe</p>
 </a>
         <a href="" class ="feature">
-            <h2>Review<h2>
+            <h2>Review</h2>
                 <p>Customer Feedback</p>
 </a>
 </div>
