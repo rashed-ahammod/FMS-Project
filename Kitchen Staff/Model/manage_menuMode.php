61001c22d9a3b4fbbdd9f406635f66d41ae75503
rashed-ahammod <ahammodr74@gmail.com>
2026-01-13 23:42:57 +0600

    image query

--- Kitchen Staff/Model/manage_menuMode.php
+++ Kitchen Staff/Model/manage_menuMode.php
@@ -5,3 +5,26 @@ function getAllMenu(){
     $sql="SELECT * FROM menu ORDER BY menu_id DESC";
     return mysqli_query($con,$sql);
 }
+
+function addMenu($data,$files){
+    $con=getConnection();
+    $name=$data['name'];
+    $price=$data['price'];
+    $description=$data['description'];
+
+    $img_name = $files['image']['name'];
+    $tmp_name = $files['image']['tmp_name'];
+
+    $img_ext = strtolower(pathinfo($img_name, PATHINFO_EXTENSION));
+    $new_name = time() . "_" . rand(1000, 9999) . "." . $img_ext;
+     $upload_path = "../../images/" . $new_name;
+
+     if(!move_uploaded_file($tmp_name,$upload_path)){
+return false;
+     }
+     $image_path=$new_name;
+        $sql = "INSERT INTO menu (name, price, description, image, availability)
+            VALUES ('$name', '$price', '$description', '$image_path', 1)";
+            return mysqli_query($con,$sql);
+
+}
