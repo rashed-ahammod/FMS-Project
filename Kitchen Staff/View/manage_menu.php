e5677959c574c07b2ad09f0288f0ccfbc530841f
rashed-ahammod <ahammodr74@gmail.com>
2026-01-13 22:24:27 +0600

    menu form

--- Kitchen Staff/View/manage_menu.php
+++ Kitchen Staff/View/manage_menu.php
@@ -4,3 +4,13 @@
         <title>Manage Menu</title>
 
 </head>
+<body>
+    <h2>Manage Menu<h2>
+
+    <form method="POST" action="" enctype="multipart/form-data">
+        <input type="text" name="name" placeholder="Food Name" required>
+        <input type="number" name="price" placeholder="Price" requied>
+        <textarea name="description" required></textarea>
+        <input type="file" name="image" required>
+        <button type="submit" name="add">Add Item</button>
+</form>
