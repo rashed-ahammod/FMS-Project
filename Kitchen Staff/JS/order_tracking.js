db9bf42b803209561995b024bf3a67ae9d4169ca
rashed-ahammod <ahammodr74@gmail.com>
2026-01-16 17:22:50 +0600

    response to php

--- Kitchen Staff/JS/order_tracking.js
+++ Kitchen Staff/JS/order_tracking.js
@@ -9,4 +9,18 @@ function updateOrder(order_id, status) {
     };
 
     xhr.send(JSON.stringify(change));
+
+        xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+
+            var data = JSON.parse(xhr.responseText);
+
+            if (data.success) {
+                document.getElementById("status" + order_id).innerText = status;
+                alert("Order updated successfully");
+            } else {
+                alert("Failed: " + (data.message || ""));
+            }
+        }
+    };
 }
