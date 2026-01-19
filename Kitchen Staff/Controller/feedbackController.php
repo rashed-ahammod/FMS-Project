c8fa03b80931c74fffa785aa62701c95b725ee08
rashed-ahammod <ahammodr74@gmail.com>
2026-01-19 18:31:44 +0600

    handle session cookie

--- Kitchen Staff/Controller/feedbackController.php
+++ Kitchen Staff/Controller/feedbackController.php
@@ -1,4 +1,18 @@
 <?php
+
+session_start();
+
+
+if (!isset($_SESSION['admin']) && isset($_COOKIE['remember_role'])) {
+    if ($_COOKIE['remember_role'] === 'admin') {
+        $_SESSION['admin'] = true;
+    }
+}
+
+if (!isset($_SESSION['admin'])) {
+    header("Location: /FMS/View/Login.php");
+    exit();
+}
 require_once '../Model/feedbackModel.php';
 
 if (isset($_POST['feedback_id']) && isset($_POST['reply'])) {
--- Kitchen Staff/View/feedback.php
+++ Kitchen Staff/View/feedback.php
@@ -1,4 +1,19 @@
 <?php
+session_start();
+
+
+if (!isset($_SESSION['admin']) && isset($_COOKIE['remember_role'])) {
+    if ($_COOKIE['remember_role'] === 'admin') {
+        $_SESSION['admin'] = true;
+    }
+}
+
+
+if (!isset($_SESSION['admin'])) {
+    header("Location: /FMS/Kitchen Staff/View/Login.php");
+    exit();
+}
+
 require_once '../Model/feedbackModel.php';
 $feedbacks = getAllFeedbacks();
 ?>
