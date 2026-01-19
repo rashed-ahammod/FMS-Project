d421e13627e835262bf221a80f9d99305ddcd440
rashed-ahammod <ahammodr74@gmail.com>
2026-01-19 18:23:03 +0600

    complete feedback

--- Kitchen Staff/View/feedback.php
+++ Kitchen Staff/View/feedback.php
@@ -21,3 +21,59 @@ $feedbacks = getAllFeedbacks();
         <th>Reply</th>
         <th>Action</th>
     </tr>
+<?php
+if ($feedbacks && mysqli_num_rows($feedbacks) > 0) {
+
+    while ($row = mysqli_fetch_assoc($feedbacks)) {
+
+        $feedbackId = $row['feedback_id'];
+        $hasReply = !empty($row['feedback_reply']);
+?>
+    <tr>
+        <td><?php echo $row['order_id']; ?></td>
+        <td><?php echo htmlspecialchars($row['customer_name']); ?></td>
+        <td><?php echo htmlspecialchars($row['feedback_message']); ?></td>
+
+        <td>
+            <?php if (!$hasReply) { ?>
+                <form method="POST" action="../Controller/feedbackController.php">
+                    <input type="hidden" name="feedback_id"
+                           value="<?php echo $feedbackId; ?>">
+
+                    <textarea name="reply" required
+                              placeholder="Write reply..."></textarea>
+        </td>
+
+        <td>
+                    <button type="submit">Reply</button>
+                </form>
+            <?php } else { ?>
+                <textarea disabled><?php
+                    echo htmlspecialchars($row['feedback_reply']);
+                ?></textarea>
+        </td>
+
+        <td>
+                <span class="replied">Replied</span>
+            <?php } ?>
+        </td>
+    </tr>
+
+<?php
+    }
+
+} else {
+?>
+    <tr>
+        <td colspan="5" style="text-align:center;">
+            No feedback found
+        </td>
+    </tr>
+<?php
+}
+?>
+
+</table>
+
+</body>
+</html>
