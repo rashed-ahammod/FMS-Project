38fecf243165b91801c5d01eb8aeb2b5ba33c016
rashed-ahammod <ahammodr74@gmail.com>
2026-01-19 15:41:31 +0600

    logout control

---
+++ Kitchen Staff/Controller/logoutController.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+
+$_SESSION = [];
+session_destroy();
+
+
+if (isset($_COOKIE['remember_email'])) {
+    setcookie("remember_email", "", time() - 3600, "/");
+}
+
+if (isset($_COOKIE['remember_role'])) {
+    setcookie("remember_role", "", time() - 3600, "/");
+}
+
+header("Location: ../View/Login.php");
+exit();
+?>
