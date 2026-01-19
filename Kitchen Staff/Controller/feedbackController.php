9214b41be59867b41d66cd85309dda8700ef242a
rashed-ahammod <ahammodr74@gmail.com>
2026-01-19 18:13:46 +0600

    connect model

---
+++ Kitchen Staff/Controller/feedbackController.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../Model/feedbackModel.php';
+
+if (isset($_POST['feedback_id']) && isset($_POST['reply'])) {
+
+    $feedback_id = intval($_POST['feedback_id']);
+    $reply = trim($_POST['reply']);
+
+    if ($reply === '') {
+        header("Location: ../View/feedback.php");
+        exit;
+    }
+
+    replyFeedback($feedback_id, $reply);
+
+}
